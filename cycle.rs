3af7e5336f04be256bf5d2d954bd79e353e6b7f4
Gaurav Tyagi <grvtyagi22@gmail.com>
2024-09-21 14:49:52 +0530

    DFS is cycle detection algo

--- cycle.rs
+++ cycle.rs
@@ -15,14 +15,44 @@ fn main() {
     .into_iter()
     .collect::<Adj>();
 
-    println!("Check Cycle:");
     println!("{data:#?}");
 
-    let p = is_cycle(data, 1);
+    println!("Check Cycle by BFS:");
+    let p = is_cycle_bfs(&data, 1);
+    println!(">> {p}");
+
+    println!("Check Cycle by DFS:");
+    let p = is_cycle_dfs(&data, 1);
     println!(">> {p}");
 }
 
-fn is_cycle(data: Adj, r: i32) -> bool {
+fn is_cycle_dfs(data: &Adj, r: i32) -> bool {
+    let n = data.len() + 1;
+    let mut s = VecDeque::new();
+    s.push_back((1, 0));
+    let mut vc = vec![0; n];
+
+    while let Some((item, parent)) = s.pop_back() {
+        if vc[item as usize] == 1 {
+            continue;
+        }
+
+        println!("{item}");
+
+        for v in data.get(&item).unwrap() {
+            if vc[*v as usize] == 1 && *v != parent {
+                return true;
+            }
+            s.push_back((*v, item));
+        }
+
+        vc[item as usize] = 1;
+    }
+
+    return false;
+}
+
+fn is_cycle_bfs(data: &Adj, r: i32) -> bool {
     let n = data.len() + 1;
 
     let mut q = VecDeque::new();
